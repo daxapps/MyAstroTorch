3ddfbd575a1ed4f2211bc341908f5fc5858b865f
Jason Crawford <daxapps777@gmail.com>
2016-07-15 17:07:17 -0500

    Udemy

--- MyAstroTorch/ViewController.swift
+++ MyAstroTorch/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var screenRed = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
+    @IBOutlet weak var myButton: UIButton!
+    
+    @IBAction func MyButtonClicked(_ sender: UIButton) {
+        
+    
+        if(screenRed) {
+            
+            self.view.backgroundColor = UIColor.blue()
+            
+    } else {
+    
+        self.view.backgroundColor = UIColor.red()
+            
+        }
+        
+        screenRed = !screenRed
+        
+    }
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
